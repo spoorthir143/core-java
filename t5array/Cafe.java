1bf290855c15bf149649220f4be23510abc4ac01
spoorthir143 <spoorthir704@gmail.com>
2026-03-05 22:19:25 +0530

    array

---
+++ t5array/Cafe.java
@@ -0,0 +1,49 @@
+class Cafe {
+	
+	
+	// tea;
+	// coffee;
+	// snack;
+	// contact;
+	
+	public static void main(String[] args){
+	
+	String cafeName= "Cringe Cafe";
+	
+		String teaNames[]={"Amruth chaha","Thaj chaha",
+		"Black chaha","Green chaha","Llemon chaha"};
+		
+		int noOfTeaNames= teaNames.length;
+		System.out.println("no of tea added are:" + noOfTeaNames);
+		
+		//System.out.println("the name of Cafe is" + cafeName);
+		System.out.println("List of TeaName are:");
+		System.out.println("List of TeaName are:");
+		// System.out.println(teaNames[0] + "\n" +
+		// teaNames[1]+"\n"+
+		// teaNames[2]+"\n"+
+		// teaNames[3]+"\n"+
+		// teaNames[4]+"\n");
+		
+		String amruth = teaNames[1];
+		System.out.println(amruth);
+		
+		String thaj = teaNames[2];
+		System.out.println(thaj);
+		
+		String black = teaNames[3];
+		System.out.println(black);
+		
+		String green = teaNames[4];
+		System.out.println(green);
+		
+		String lemon = teaNames[5];
+		System.out.println(lemon);
+		
+		//for(String teaName:teaNames)
+		//	System.out.println(teaName);
+		
+		
+	
+	}
+}
---
+++ t5array/WorldCup.java
@@ -0,0 +1,63 @@
+class WorldCup
+{
+	public static void main(String[] args)
+	{
+		String groupA[]={"India","United States of America","Namibia","Netherlands","Pakistan"};
+		String groupB[]={"Australia","Sri Lanka","Zimbabwe","Ireland","Oman"};
+		String groupC[]={"England","West Indies","Bangladesh","Nepal","Italy"};
+		String groupD[]={"New Zealand","South Africa","Afghanistan","Canada","United Arab Emirates"};
+		
+		
+		String india=groupA[0];
+		System.out.println(india);
+		String unitedStatesOfAmerica=groupA[1];
+		System.out.println(unitedStatesOfAmerica);
+		String namibia=groupA[2];
+		System.out.println(namibia);
+		String netherlands=groupA[3];
+		System.out.println(netherlands);
+		String pakistan=groupA[4];
+		System.out.println(pakistan);
+		
+	
+		
+		String australia=groupB[0];
+		System.out.println(australia);
+		String sriLanka=groupB[1];
+		System.out.println(sriLanka);
+		String zimbabwe=groupB[2];
+		System.out.println(zimbabwe);
+		String ireland1=groupB[3];
+		System.out.println(ireland1);
+		String oman=groupB[4];
+		System.out.println(oman);
+
+		
+		
+		String england=groupC[0];
+		System.out.println(england);
+		String westIndies=groupC[1];
+		System.out.println(westIndies);
+		String bangladesh=groupC[2];
+		System.out.println(bangladesh);
+		String nepal=groupC[3];
+		System.out.println(nepal);
+		String italy=groupC[4];
+		System.out.println(italy);
+
+		
+
+		String newZealand=groupD[0];
+		System.out.println(newZealand);
+		String southAfrica=groupD[1];
+		System.out.println(southAfrica);
+		String afghanistan=groupD[2];
+		System.out.println(afghanistan);
+		String canada=groupD[3];
+		System.out.println(canada);
+		String unitedArabEmirates=groupD[4];
+		System.out.println(unitedArabEmirates);
+
+
+	}
+}
