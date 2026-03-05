bcee7b8b83201434eb36db95383ee00f0f618fcb
spoorthir143 <spoorthir704@gmail.com>
2026-03-05 22:35:22 +0530

    literals

---
+++ arrayleteral/SonyLive.java
@@ -0,0 +1,24 @@
+class SonyLive{
+	public static void main(String[] args){
+	//String[] entertainment = {
+	//	"Golmaal","Golmaal Returns","Golmaal 3","Golmaal Again","Dhamaal","Double Dhamaal","Total Dhamaal","Hera Pheri","Phir Hera Pheri","Hungama",
+	//	"Hungama 2","Chup Chup Ke","De Dana Dan","Welcome","Welcome Back","Bhool Bhulaiyaa","Bhool Bhulaiyaa 2","Housefull","Housefull 2","Housefull 3",
+	//	"Housefull 4","Andaz Apna Apna","Fukrey","Fukrey Returns","Dream Girl","Badhaai Ho","Good Newwz","OMG","Pati Patni Aur Woh","Partner"
+	//	};
+	
+	String golmaal="Golmaal";
+	String dhamaal="Dhamaal";
+	String hungama="Hungama";
+	String chupChupke="Chup Chup Ke";
+	String deDanaDan="De Dana Dan";
+	String welcome="Welcome";
+	String welcomeBack="Welcome Back";
+	
+	String[] entertainments = {
+	"Golmaal","Golmaal Returns","Golmaal 3","Golmaal Again","Dhamaal","Double Dhamaal","Total Dhamaal","Hera Pheri","Phir Hera Pheri","Hungama",
+		"Hungama 2","Chup Chup Ke","De Dana Dan","Welcome","Welcome Back"};
+		
+		for(String entert:entertainments)
+		System.out.println(entert);
+	}
+}
