7f6073314d3c165c8f2dba53541359660e22265b
spoorthir143 <spoorthir704@gmail.com>
2026-03-05 22:14:16 +0530

    task3

---
+++ task3/Clock.java
@@ -0,0 +1,33 @@
+class Clock
+{
+	static String brand	;
+static String colour ;	
+static String productDimensions	;
+static String itemWeight ;	
+static String numberOfItems ;
+static String scent	;
+static String areBatteriesIncluded ;
+static String indoorOutdoorUsage;
+static String shape;
+	public static void main(String []clock)
+	{
+brand =	"Aromahpure";
+System.out.println("brand:"+brand);
+colour = "WHITE";
+System.out.println("colour:"+colour);
+productDimensions ="5.6W x 6.5H Centimeters";
+System.out.println("productDimensions:"+productDimensions);
+itemWeight ="430 Grams";
+System.out.println("itemWeight:"+itemWeight);
+numberOfItems	= "2";
+System.out.println("numberOfItems:"+numberOfItems);
+scent =	"caramel";
+System.out.println("scent:"+scent);
+areBatteriesIncluded="No";
+System.out.println("areBatteriesIncluded:"+areBatteriesIncluded);
+indoorOutdoorUsage ="Indoor";
+System.out.println("indoorOutdoorUsage:"+indoorOutdoorUsage);
+shape =	"Round";
+System.out.println("shape:"+shape);
+	}
+}
